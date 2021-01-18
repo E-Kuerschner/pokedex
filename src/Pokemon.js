ef687d5e0b8a365e56724ae7940605a2802f354f
Erich Kuerschner <erichk@vailsys.com>
2021-01-17 20:57:34 -0600

    ensure most recent pokemon is on top

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -4,12 +4,15 @@ import PokemonInfo from "./PokemonInfo";
 
 import { Card } from "./styles/components";
 
-const Pokemon = ({ name, url, image, num }) => {
+const Pokemon = ({ name, url, image, num, onOpenClose }) => {
   const [hidden, setHidden] = useState(true);
+  const [orderOpened, setOrderOpened] = useState(1);
 
   // handling hidden components
   const handleClickHidden = () => {
     setHidden(!hidden);
+    const numberOpen = onOpenClose(num);
+    setOrderOpened(numberOpen);
   };
 
   return (
@@ -17,7 +20,7 @@ const Pokemon = ({ name, url, image, num }) => {
       <img src={image} alt={`${name}`}></img>
       <p>{name}</p>
       {!hidden ? (
-        <PokemonInfo name={name} url={url} image={image} num={num} />
+        <PokemonInfo name={name} url={url} image={image} num={num} order={orderOpened} />
       ) : null}
     </Card>
   );
